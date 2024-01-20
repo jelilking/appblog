ac6aa6e7bdd01220aa06e664ca78b15ee35cb583
jelilking <abduljelilozigi@gmail.com>
2024-01-20 23:46:36 +0100

    updated api

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -13,10 +13,10 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const router = Router();
 
+router.get("/", getAuthors);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/:id", getUser);
-router.get("/", getAuthors);
 router.post("/change-avatar", authMiddleware, changeAvatar);
 router.patch("/edit-user", authMiddleware, editUser);
 
